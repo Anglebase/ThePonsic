190e8a0b2a4e5ef2f22b0bd1ed87e19563c8a30e
Anglebase <1628818324@qq.com>
2025-04-02 22:38:03 +0800

    完善绘图函数

---
+++ ponsic-winsafe/examples/context_2d.rs
@@ -0,0 +1,58 @@
+use ponsic_winsafe::{
+    graphics::context_2d::{Context2D, Point, Rect},
+    wndproc, *,
+};
+
+fn paint(context: Context2D) {
+    context.clear();
+    context.line(Point::from_xy(10, 10), Point::from_xy(200, 200));
+    let points = [
+        Point::from_xy(10, 5),
+        Point::from_xy(200, 20),
+        Point::from_xy(30, 300),
+    ];
+    context.polyline(&points);
+
+    let rect = Rect {
+        left: 400,
+        top: 20,
+        right: 450,
+        bottom: 100,
+    };
+    let p1 = Point::from_xy(rect.left, rect.top);
+    let p2 = Point::from_xy(rect.right, rect.bottom);
+    context.arc(rect, p1, p2);
+    context.polyline(&rect.to_polyline());
+}
+
+fn process(Events { event, .. }: Events) -> Option<isize> {
+    match event {
+        Event::Destroy => {
+            App::should_exit(0);
+            Some(0)
+        }
+        Event::Paint { context } => {
+            paint(context.into());
+            Some(0)
+        }
+        _ => None,
+    }
+}
+
+fn main() {
+    let class = Registrar::new("MyApp")
+        .set_cursor(Cursor::Arrow)
+        .set_process(wndproc!(process))
+        .build()
+        .unwrap();
+
+    let window = class
+        .window_builder(100, 100, 800, 600)
+        .set_title("MyApp")
+        .set_style(&[WindowStyle::OverlappedWindow])
+        .build()
+        .unwrap();
+
+    window.show();
+    App::handle_events();
+}
--- ponsic-winsafe/src/graphics/context_2d/mod.rs
+++ ponsic-winsafe/src/graphics/context_2d/mod.rs
@@ -1,7 +1,14 @@
+mod types;
+use std::ptr::null_mut;
+
 use super::context::Context;
+pub use types::*;
 use winapi::{
     shared::windef::{HDC, HWND},
-    um::winuser::{BeginPaint, COLOR_WINDOW, EndPaint, FillRect, PAINTSTRUCT},
+    um::{
+        wingdi::{Arc, LineTo, MoveToEx, Polyline},
+        winuser::{BeginPaint, COLOR_WINDOW, EndPaint, FillRect, PAINTSTRUCT},
+    },
 };
 
 pub struct Context2D {
@@ -31,6 +38,37 @@ impl Context2D {
     pub fn clear(&self) {
         unsafe {
             FillRect(self.hdc, &self.ps.rcPaint, (COLOR_WINDOW + 1) as _);
+        }
+    }
+}
+
+impl Context2D {
+    pub fn line(&self, p1: Point, p2: Point) {
+        unsafe {
+            MoveToEx(self.hdc, p1.x, p1.y, null_mut());
+            LineTo(self.hdc, p2.x, p2.y);
+        }
+    }
+
+    pub fn polyline(&self, points: &[Point]) {
+        unsafe {
+            Polyline(self.hdc, points.as_ptr() as _, points.len() as _);
+        }
+    }
+
+    pub fn arc(&self, border: Rect, p1: Point, p2: Point) {
+        unsafe {
+            Arc(
+                self.hdc,
+                border.left,
+                border.top,
+                border.right,
+                border.bottom,
+                p1.x,
+                p1.y,
+                p2.x,
+                p2.y,
+            );
         }
     }
 }
