76cdc7f8965c30e3bc63de8dd0a788ffb248ee47
Anglebase <1628818324@qq.com>
2025-04-03 13:28:20 +0800

    添加多窗口示例

---
+++ ponsic-winsafe/examples/multwindows.rs
@@ -0,0 +1,53 @@
+use std::thread::spawn;
+
+use ponsic_winsafe::{wndproc, *};
+
+fn process(Events { event, window }: Events) -> Option<isize> {
+    match event {
+        Event::Destroy => {
+            App::should_exit(0);
+            Some(0)
+        }
+        Event::Other { .. } => None,
+        e @ _ => {
+            println!("{}", window.title());
+            println!("{:?}", e);
+            None
+        }
+    }
+}
+
+fn main() {
+    let class = Registrar::new("MyApp")
+        .set_cursor(Cursor::Arrow)
+        .set_process(wndproc!(process))
+        .build()
+        .unwrap();
+
+    let window = class
+        .window_builder(100, 100, 800, 600)
+        .set_title("MyApp")
+        .set_style(&[WindowStyle::OverlappedWindow])
+        .build()
+        .unwrap();
+
+    window.show();
+    let id = window.id();
+
+    let join = spawn(move || {
+        let window = class
+            .window_builder(100, 100, 400, 300)
+            .set_title("MyApp2")
+            .set_parent(id)
+            .set_style(&[WindowStyle::OverlappedWindow])
+            .build()
+            .unwrap();
+
+        window.show();
+
+        App::handle_events();
+    });
+
+    App::handle_events();
+    join.join().unwrap();
+}
