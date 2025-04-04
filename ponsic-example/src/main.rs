9d0aca172993e9af12c50da8361676d2eedb0acb
Anglebase <1628818324@qq.com>
2025-04-04 13:05:50 +0800

    寻找最佳实践

--- ponsic-example/src/main.rs
+++ ponsic-example/src/main.rs
@@ -1,11 +1,11 @@
 use gom::Registry;
-use ponsic::{App, WindowManager, WindowStyle};
+use ponsic::{graphics::context_2d::Rect, App, WindowManager, WindowStyle};
 use ponsic_example::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let window = MAINWINDOW_CLASS
-        .window_builder(10, 10, 800, 600)
+        .window_builder(Rect::from_ps(10, 10, 800, 600))
         .set_title("Ponsic 应用程序")
         .set_style(&[WindowStyle::OverlappedWindow])
         .build()?;
@@ -13,7 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Registry::register("MainWindow", window.id()).unwrap();
 
     let button = BUTTON_CLASS
-        .window_builder(10, 10, 100, 50)
+        .window_builder(Rect::from_ps(10, 10, 100, 50))
         .set_parent(window.id())
         .set_style(&[WindowStyle::Child])
         .set_title("Button")
