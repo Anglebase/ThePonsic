4fe1667b2afba27d567206845f99dad861c237c8
Anglebase <1628818324@qq.com>
2025-04-07 19:30:43 +0800

    优化示例

--- ponsic/examples/push_button.rs
+++ ponsic/examples/push_button.rs
@@ -6,12 +6,25 @@ use ponsic::{
 
 fn main() -> Result<(), SystemError> {
     let main_window = MainWindow::new(Rect::from_ps(100, 100, 800, 600), "New Ponsic Window")?;
-    let mut button = PushButton::new(Rect::from_ps(50, 50, 100, 50), "PushButton", &main_window)?;
-    button.add_callback(|b| {
-        println!("PushButton pressed!: {}", b);
-    });
+    let mut buttons = vec![];
+
+    for i in 0..8 {
+        for j in 0..12 {
+            let mut button = PushButton::new(
+                Rect::from_ps(i * 90 + 10, j * 45 + 10, 80, 40),
+                &format!("Button {}x{}", i, j),
+                &main_window,
+            )?;
+            button.add_callback(move |b| {
+                println!("Button {}x{} pressed!: {}", i, j, b);
+            });
+            buttons.push(button);
+        }
+    }
+
     main_window.show();
-    button.show();
+    buttons.iter_mut().for_each(|b| b.show());
+
     while App::handle_event(true).unwrap_or(true) {}
     Ok(())
 }
