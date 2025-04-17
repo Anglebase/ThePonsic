6419487223fb818116f173edba279d876e2a9237
Anglebase <1628818324@qq.com>
2025-04-17 23:53:56 +0800

    更新项目结构

--- ponsic-example/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(target_os = "windows")]
-mod button;
-#[cfg(target_os = "windows")]
-mod mainwindow;
-
-#[cfg(target_os = "windows")]
-pub use button::Button;
-#[cfg(target_os = "windows")]
-pub use mainwindow::MainWindow;
