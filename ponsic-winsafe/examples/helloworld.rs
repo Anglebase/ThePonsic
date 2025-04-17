55cd28b5309a272722cc29c52175def23c31c599
Anglebase <1628818324@qq.com>
2025-04-17 23:26:22 +0800

    添加Hello Ponsic示例代码

---
+++ ponsic-winsafe/examples/helloworld.rs
@@ -0,0 +1,30 @@
+use ponsic_winsafe::*;
+
+fn main() -> Result<(), SystemError> {
+    let class = Registrar::new("HelloWorld")
+        .set_cursor(Cursor::Arrow)
+        .set_process(wndproc!(();|Events { event,.. }|{
+            if let Event::Mouse { button: Button::Left, status: ButtonStatus::Down, .. } = event {
+                println!("Hello Ponsic!");
+                return Return::Finish
+            }
+            if let Event::Window(WindowEvent::Destroy) = event {
+                App::should_exit(0);
+                return Return::Finish
+            }
+            Return::Default
+        }))
+        .build()?;
+
+    let window = class
+        .make_window(Rect::from((Point::new(100, 100), Size::new(800, 600))))
+        .set_style(&[WindowStyle::OverlappedWindow])
+        .set_title("Hello Ponsic!")
+        .build()?;
+
+    window.show();
+
+    while App::handle_event(true).unwrap() {}
+
+    Ok(())
+}
